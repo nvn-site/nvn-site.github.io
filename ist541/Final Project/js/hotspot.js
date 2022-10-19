85a45e6ab06d5dfc93ab581e1e57803ef6f37f38
Nichol-veles <89709986+Nichol-Veles@users.noreply.github.com>
2022-10-18 18:30:02 -0700

    Update hotspot.js

--- ist541/Final Project/js/hotspot.js
+++ ist541/Final Project/js/hotspot.js
@@ -1,111 +1,29 @@
-<script src="js/hotspot.js"></script>
-<div id="Sink" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="gridSystemModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="gridSystemModalLabel"> 
-           SD-20
-        </h4>
-      </div>
-      <div class="modal-body">
-        <div id="feedback"></div>
-      </div><!-- /.modal-body -->
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
-</div><!-- /.modal --><script src="js/hotspot.js"></script>
-<div id="Sink" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="gridSystemModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="gridSystemModalLabel"> 
-           SD-20
-        </h4>
-      </div>
-      <div class="modal-body">
-        <div id="feedback"></div>
-      </div><!-- /.modal-body -->
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
-</div><!-- /.modal --><script src="js/hotspot.js"></script>
-<div id="Sink" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="gridSystemModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="gridSystemModalLabel"> 
-           SD-20
-        </h4>
-      </div>
-      <div class="modal-body">
-        <div id="feedback"></div>
-      </div><!-- /.modal-body -->
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
-</div><!-- /.modal --><div id="myModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="gridSystemModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="gridSystemModalLabel"> 
-           The sink, table and trash are all cleaned using SD-20. While the stainless steel refrigerator is cleaned with Consume Eco-Lyzer.
-        </h4>
-      </div>
-      <div class="modal-body">
-        <div id="feedback"></div>
-      </div><!-- /.modal-body -->
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
-</div><!-- /.modal --><div id="myModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="gridSystemModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="gridSystemModalLabel"> 
-           The sink, table and trash are all cleaned using SD-20. While the stainless steel refrigerator is cleaned with Consume Eco-Lyzer.
-        </h4>
-      </div>
-      <div class="modal-body">
-        <div id="feedback"></div>
-      </div><!-- /.modal-body -->
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
-</div><!-- /.modal --><div id="myModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="gridSystemModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="gridSystemModalLabel"> 
-           The sink, table and trash are all cleaned using SD-20. While the stainless steel refrigerator is cleaned with Consume Eco-Lyzer.
-        </h4>
-      </div>
-      <div class="modal-body">
-        <div id="feedback"></div>
-      </div><!-- /.modal-body -->
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->$( function() {
+// JavaScript Document
+$( function() {
    $("map area").click( function(){
 
-      if ($(this).attr("id") == "USA") {
+      if ($(this).attr("id") == "Refrigerator") {
          $("#feedback").css("color","red");
-         $("#feedback").html("No, USA is the third one");
+         $("#feedback").html("The refrigerator is cleaned with Consume Eco-Lyzer and the white towel.");
       }
 
-      if ($(this).attr("id") == "China") {
+      if ($(this).attr("id") == "Table") {
          $("#feedback").css("color","green");
-         $("#feedback").html("Yes, China has the largest population: 1,343,239,923");
+         $("#feedback").html("The table is cleaned with SD-20 and the green towel.");
       }
 
-      if ($(this).attr("id") == "India") {
+      if ($(this).attr("id") == "Trash") {
          $("#feedback").css("color","red");
-         $("#feedback").html("No,India is the second one");
+         $("#feedback").html("The table is cleaned with SD-20 and the green towel.");
       }
 
+	 if ($(this).attr("id") == "Sink") {
+         $("#feedback").css("color","red");
+         $("#feedback").html("The table is cleaned with Consume Eco-Lyzer and the white towel. But the counter is cleaned with SD_20 and the green towel.");
+      }  
   $("#feedback").css("backgroundColor","yellow");
-$('#myModal').modal("show");
 
    });
 }); //end main jQuery function
-// JavaScript Document
+
+
